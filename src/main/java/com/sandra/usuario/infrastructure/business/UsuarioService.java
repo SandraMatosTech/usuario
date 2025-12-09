1d23467dea1ef7186fd5f5002154bbf44485704a
SandraMatosTech <matos.sandrarodrigues@hotmail.com>
2025-12-09 15:51:10 -0300

    correção de pasta

--- src/main/java/com/sandra/usuario/infrastructure/business/UsuarioService.java
+++ src/main/java/com/sandra/usuario/infrastructure/business/UsuarioService.java
@@ -1,8 +1,8 @@
 package com.sandra.usuario.infrastructure.business;
 
 
-import com.sandra.usuario.businness.dto.UsuarioDTO;
 import com.sandra.usuario.infrastructure.business.converter.UsuarioConverter;
+import com.sandra.usuario.infrastructure.business.dto.UsuarioDTO;
 import com.sandra.usuario.infrastructure.entity.Usuario;
 import com.sandra.usuario.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
